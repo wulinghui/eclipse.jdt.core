0f2de082695e9defa16d38958f03e40135a1d499
Stephan Herrmann <stephan.herrmann@berlin.de>
2013-11-21 11:08:30 +0100

    A note on where to fix the NPE observed, e.g., in GTT.test0931()

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/Scope.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/Scope.java
@@ -343,7 +343,7 @@ public abstract class Scope {
 						if (wideType.arguments == null)
 							continue; // assume we already have an error here
 						if (!narrowType.isProperType() || !wideType.isProperType())
-							continue; // skip the following check if inference variables are involved
+							continue; // skip the following check if inference variables are involved FIXME(SH): also exclude uninitialized CaptureBinding18 anywhere inside
 						int numTypeArgs = wideType.arguments.length;
 						TypeBinding[] bounds = new TypeBinding[numTypeArgs];
 						for (int k = 0; k < numTypeArgs; k++) {
