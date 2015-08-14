d3b9b937de514b4fd3b10cbc0a34d489146f25f3
Mateusz Matela <mateusz.matela@gmail.com>
2015-08-14 21:38:48 +0200

    fixed bug 471780: [formatter] Regression in enum value Javadoc formatting
    Change-Id: I2e59bbe4c5e0f63c2813746a3f8a47e965cf3777
    Signed-off-by: Mateusz Matela <mateusz.matela@gmail.com>

--- org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
+++ org.eclipse.jdt.core.tests.model/src/org/eclipse/jdt/core/tests/formatter/FormatterBugsTests.java
@@ -11161,4 +11161,17 @@ public void testBug472009() {
 		"}";
 	formatSource(source);
 }
+/**
+ * @bug 471780 - [formatter] Regression in enum value Javadoc formatting
+ */
+public void testBug471780() {
+	String source = 
+		"public enum MyEnum {\r\n" + 
+		"	/** A. */\r\n" + 
+		"	A,\r\n" + 
+		"	/** B. */\r\n" + 
+		"	B\r\n" + 
+		"}";
+	formatSource(source);
+}
 }
